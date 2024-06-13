9a9fe5dfb2b87fcaae6dc7f1ed97c97caa140c97
yamini <manemyamani003@gmail.com>
2024-06-13 22:44:04 +0530

    tasks

--- PHASE2/touch.sh
+++
@@ -1,8 +0,0 @@
-git init
-git remote add origin https://github.com/yedotimahesh/SMART-ATTANDANCE-SYSTEM-.git
-git remote -v  --force
-git add . --force
-git commit -m "upload1"
-
-
-git push -f origin master
